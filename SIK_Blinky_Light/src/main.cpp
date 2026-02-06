8f432d0dffac135630f15d3889b7a9caa458e3f1
Dean J Throop <Dean.Throop@GMail.com>
2026-02-05 17:30:16 -0800

    initial setup

---
+++ SIK_Blinky_Light/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+    // put your setup code here, to run once:
+    //int result = myFunction(2, 3);
+    pinMode(13, OUTPUT);      // Set pin 13 to output
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(13, HIGH);   // Turn on the LED
+
+    delay(100);              // Wait for two seconds
+
+    digitalWrite(13, LOW);    // Turn off the LED
+
+    delay(5000);              // Wait for two seconds
+
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+    return x + y;
+}
